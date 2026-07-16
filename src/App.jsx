603a7a196d6ce9c8855cb9a563f854cf7a77452d
vishu krish <me@example.com>
2026-07-16 19:25:07 +0300

    new ip camera

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import BaristaView from "./components/BaristaView";
 import CustomerView from "./components/CustomerView";
 import SensorSimulator from "./components/SensorSimulator";
 import CameraDetector from "./components/CameraDetector";
-import PhoneCamera from "./components/PhoneCamera";
 
 export default function App() {
   return (
@@ -12,7 +11,6 @@ export default function App() {
       <Route path="/barista" element={<BaristaView />} />
       <Route path="/simulator" element={<SensorSimulator />} />
       <Route path="/camera-detect" element={<CameraDetector />} />
-      <Route path="/phone-camera" element={<PhoneCamera />} />
     </Routes>
   );
 }
